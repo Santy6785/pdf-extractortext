146f3c34f96a78b9b5925b236f16a717eaf15438
Santiago Dorado <santiagodorado2504@gmail.com>
2026-09-06 21:30:05 -0300

    chore: eliminar app/file_manager.py fuera de uso

--- app/file_manager.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def archivo_existe(ruta):
-    return os.path.exists(ruta)
-
-def guardar_txt(texto, nombre_archivo):
-    with open(nombre_archivo, "w", encoding="utf-8") as f:
-        f.write(texto)
